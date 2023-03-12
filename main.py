f82637e94cfdad8334012cb15126506507185a68
Wioletta Dziwińska <wioletta.dziwinska@student.wat.edu.pl>
2023-03-12 17:54:49 +0100

    Multiple face detection

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ emotion_dict = {
 # Wczytanie kaskady klasyfikatorów Haar do wykrywania twarzy
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-def rectangleBackground():
+def rectangleBackground(x, y, w, h):
     # Utworzenie przezroczystego prostokąta
     color = (0, 0, 0)  # kolor czarny
     x, y = 0, 0  # położenie lewego górnego rogu
@@ -68,15 +68,39 @@ while True:
     labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
     domainEmotion = labels[emotion_index]
 
-    rectangleBackground()
-    faceRectangle()
-
-    # wyświetlenie wyniku na obrazie
-    text = f"Emotion: {domainEmotion}, \n probability: {emotion_probability}"
-    cv2.putText(frame, domainEmotion, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.85, (255, 255, 255), 2, cv2.LINE_AA)
-
     emotion_text = ""
-    for i, probability in enumerate(emotion_probability):
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Wykrycie twarzy na ramce
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    # Wykrycie każdej twarzy
+    for (x, y, w, h) in faces:
+        face = gray[y:y + h, x:x + w]
+        face = cv2.resize(face, (48, 48))
+        face = face / 255.0
+        face = face.reshape(1, 48, 48, 1)
+        prediction = model.predict(face)[0]
+
+        # Znalezienie indeksu klasy z najwyższą wartością prawdopodobieństwa
+        max_index = int(np.argmax(prediction))
+
+        rectangleBackground(x, y, w, -35)
+        # Wypisanie emocji na obrazie
+        cv2.putText(frame, labels[max_index], (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 0, 255), 2)
+        # cv2.putText(frame, domainEmotion, (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.85, (255, 255, 255), 2, cv2.LINE_AA)
+
+        rectangleBackground(0, 0, 170, 200)
+
+        for i, probability in enumerate(prediction):
+            emotion = emotion_dict[i]
+            probability = round(probability * 100, 2)
+            emotion_text = f" {emotion}: {probability}%  "
+
+            cv2.putText(frame, emotion_text, (5, 25 + i * 25), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (255, 255, 255), 1,
+                        cv2.LINE_AA)
         textDsplaying(i, probability)
 
     cv2.imshow("Emotion Recognition", frame)
