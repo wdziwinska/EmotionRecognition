4fe25e36be6780b55ceb1dee2dac0cbdb579eb88
Wioletta Dziwińska <wioletta.dziwinska@student.wat.edu.pl>
2023-03-14 21:08:32 +0100

    Removing useless code

--- siec.py
+++ siec.py
@@ -5,23 +5,19 @@ from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 # definicja modelu
 model = Sequential()
 
-# warstwa konwolucyjna
+# warstwy konwolucyjne
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1)))
 model.add(MaxPooling2D((2, 2)))
 
-# warstwa konwolucyjna
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2, 2)))
 
-# warstwa konwolucyjna
 model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2, 2)))
 
-# warstwa konwolucyjna
 model.add(Conv2D(256, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2, 2)))
 
-# warstwa wypłaszczająca
 model.add(Flatten())
 
 # warstwa ukryta
@@ -41,7 +37,7 @@ data_gen = ImageDataGenerator(rescale=1./255)
 
 # wczytanie danych treningowych za pomocą generatora - wykorzystanie generatora do wczytania danych treningowych
 train_data = data_gen.flow_from_directory("train_fer", target_size=(48, 48), color_mode='grayscale', batch_size=32, class_mode='categorical')
-# trenowanie modelu na danych treningowych przez 50 epok
+# trenowanie modelu na danych treningowych przez 150 epok
 model.fit(train_data, epochs=150)
 
 model.save('emotions_detection_fer_model.h5')
