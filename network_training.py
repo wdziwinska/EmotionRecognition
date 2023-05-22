4be469f93977a514404bab4075527b6ae6026a76
Wioletta Dziwińska <55066543+wdziwinska@users.noreply.github.com>
2023-05-22 11:32:25 +0200

    Script that achieved the best results

--- network_training.py
+++ network_training.py
@@ -1,82 +1,118 @@
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation, BatchNormalization
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+
+EMOTIONS = ["angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"]
 
 # definicja modelu
 model = Sequential()
 
-# warstwy konwolucyjne
-model.add(Conv2D(32, (3, 3), input_shape=(48, 48, 1)))
+model.add(Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal',  input_shape=(48, 48, 1)))
 model.add(BatchNormalization())
 model.add(Activation("relu"))
-# model.add(Conv2D(32, (3, 3)))
-# model.add(BatchNormalization())
-# model.add(Activation("relu"))
+model.add(Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
 model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.1))
+model.add(Dropout(0.2))
 
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(128, (3, 3), padding='same', kernel_initializer='he_normal'))
 model.add(BatchNormalization())
 model.add(Activation("relu"))
-# model.add(Conv2D(64, (3, 3)))
-# model.add(BatchNormalization())
-# model.add(Activation("relu"))
+model.add(Conv2D(128, (3, 3), padding='same', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Conv2D(128, (3, 3), padding='same', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
 model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.1))
+model.add(Dropout(0.2))
 
-model.add(Conv2D(128, (3, 3)))
+model.add(Conv2D(256, (3, 3), padding='same', kernel_initializer='he_normal'))
 model.add(BatchNormalization())
 model.add(Activation("relu"))
-# model.add(Conv2D(128, (3, 3)))
-# model.add(BatchNormalization())
-# model.add(Activation("relu"))
-# model.add(Conv2D(64, (3, 3)))
-# model.add(BatchNormalization())
-# model.add(Activation("relu"))
+model.add(Conv2D(256, (3, 3), padding='same', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Conv2D(256, (3, 3), padding='same', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
 model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.1))
+model.add(Dropout(0.2))
 
-model.add(Conv2D(256, (3, 3)))
+model.add(Conv2D(512, (3, 3), padding='same', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Conv2D(512, (3, 3), padding='same', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Conv2D(512, (3, 3), padding='same', kernel_initializer='he_normal'))
 model.add(BatchNormalization())
 model.add(Activation("relu"))
 model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.1))
-# #
-# model.add(Conv2D(256, (3, 3)))
-# model.add(BatchNormalization())
-# model.add(Activation("relu"))
-# model.add(MaxPooling2D((2, 2)))
-# model.add(Dropout(0.2))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(1024, (3, 3), padding='same', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Conv2D(1024, (3, 3), padding='same', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Conv2D(1024, (3, 3), padding='same', kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.2))
 
 #warstwa spłaszczająca
 model.add(Flatten())
 
 # warstwa gęsta
-model.add(Dense(256))
+model.add(Dense(128, kernel_initializer='he_normal'))
 model.add(BatchNormalization())
 model.add(Activation("relu"))
-#przedziwdziała przetrenowaniu -  losowo wybiera, które neurony będą ignorowane w trakcie jednej iteracji treningu
 model.add(Dropout(0.5))
 
-# model.add(Dense(128))
-# model.add(BatchNormalization())
-# model.add(Activation("relu"))
-# model.add(Dropout(0.5))
-
-EMOTIONS = ["angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"]
+model.add(Dense(128, kernel_initializer='he_normal'))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Dropout(0.5))
 
 # warstwa wyjściowa
 model.add(Dense(len(EMOTIONS), activation='softmax'))
 
-# kompilowanie modelu używając optymalizatora Adam, funkcji straty (f. kosztu) categorical_crossentropy (dla problemów klasyfikacji wieloklasowej) i metryki accuracy (dokładności).
+# kompilowanie modelu
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# utworzenie generatora danych treningowych
-data_gen = ImageDataGenerator(rescale=1./255)
+# utworzenie generatorów danych treningowych i walidacyjnych
+data_gen_train = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+data_gen_val = ImageDataGenerator(rescale=1./255)
 
-# wczytanie danych treningowych za pomocą generatora - wykorzystanie generatora do wczytania danych treningowych
-train_data = data_gen.flow_from_directory("train_fer", target_size=(48, 48), color_mode='grayscale', batch_size=32, class_mode='categorical')
-# trenowanie modelu na danych treningowych przez 150 epok
-model.fit(train_data, epochs=200)
+# wczytanie danych
+train_data = data_gen_train.flow_from_directory("train", target_size=(48, 48), color_mode='grayscale', batch_size=32, class_mode='categorical')
+validation_data = data_gen_val.flow_from_directory("test", target_size=(48, 48), color_mode='grayscale', batch_size=32, class_mode='categorical')
 
-model.save('emotions_detection_fer_model_new6_dense128_wiecej_konw.h5')
+# callbacki
+checkpoint = ModelCheckpoint('emotions_detection_fer_mod.h5', monitor='val_accuracy', save_best_only=True, verbose=1)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, min_delta=0.0001)
+
+# trenowanie modelu na danych treningowych przez 100 epok
+model.fit(
+    train_data,
+    epochs=100,
+    validation_data=validation_data,
+    callbacks=[checkpoint, reduce_lr]
+)
